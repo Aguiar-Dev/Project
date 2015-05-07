cf81d395b8c119e3007be0d1d6b0b0e55d9181ae
FernandoAguiarGuevarez <flaguiarguevarez@fullsail.edu>
2015-05-07 12:16:22 -0400

    Day 2 Commit

---
+++ Day2/JS/js.js
@@ -0,0 +1,24 @@
+/*
+ Fernando Luis Aguiar Guevarez
+ SDI 1505
+ Day Two
+ Fun with Data Types
+ */
+
+//This is a single line comment
+
+//These are my variables
+var myHomeAddress = "Mitchell Hammock Street";
+var milesToStore = 8.5;
+var construction;
+var myFavoriteStore = "Gamestop";
+
+//adding my confirm
+//confirm()
+var construction = confirm("Is there construction? Click OK for yes. Cancel for no.");
+
+//My outputs
+console.log("I live at " + myHomeAddress + ".");
+console.log("I shop at " + myFavoriteStore + ".")
+console.log("It is only " + milesToStore + " miles away from my home.");
+console.log("It's " + construction + " That there is construction enroute.")

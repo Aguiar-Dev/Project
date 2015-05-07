c490260f35d0c4a3739f686a794ee54b76c9143d
FernandoAguiarGuevarez <flaguiarguevarez@fullsail.edu>
2015-05-07 14:34:50 -0400

    AguiarGuevarez_FernandoAguiar_Lab2

---
+++ sdi-lab2/AguiarGuevarez_FernandoLuis_Lab2.js
@@ -0,0 +1,14 @@
+alert("JavaScript works!");
+
+//My Variables
+var storyPart = "I flew on a plane to the U.S. instead of going by boat";
+var continuation = "the plane flew very high in the air just like a \"Dragoon \" Of some sort";
+
+var weathertype = confirm("Was there bad weather? Click OK for yes, click Cancel for no.");
+var flightTime = prompt("How long do you think the flight was? In minutes.");
+
+//Output
+console.log("In 2013 " + storyPart + ".");
+console.log("From my point of view " + continuation + ".");
+console.log("You say that the flight lasted " + flightTime + " minutes. But in truth it lasted 140 minutes.");
+console.log("It is " + weathertype + " that the weather was bad that day.");
